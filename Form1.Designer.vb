8c16476d3cefaff882ef555a493f8dd67d857599
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-18 10:21:36 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Label2 = New Label()
+        PictureBox1 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(325, 132)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(325, 225)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(145, 140)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(130, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "Angle at circumference"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(102, 225)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(173, 23)
+        Button1.TabIndex = 3
+        Button1.Text = "Angle at centre"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Stencil", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(102, 54)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(518, 36)
+        Label2.TabIndex = 4
+        Label2.Text = "Theorems of a Cicle : " & vbCrLf & "Angle at centre is twice the angle at the circumference"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.download__1_
+        PictureBox1.Location = New Point(487, 140)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(199, 206)
+        PictureBox1.TabIndex = 5
+        PictureBox1.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.RosyBrown
+        ClientSize = New Size(800, 450)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b As Integer
+        a = Val(TextBox1.Text)
+        b = a * 2
+
+        TextBox2.Text = b
+
+
+    End Sub
+End Class
